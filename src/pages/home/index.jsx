7563e977682c41cf2ca04189839a0bb66e1eef1b
muhammadtanko <teekay014@gmail.com>
2024-05-25 16:56:04 +0100

    more minor changes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
+
+
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/login");
+    };
     return (
-       <div className="text-center font-extrabold text-4xl">
-        HOME
-       </div>
+        <div className="flex flex-col gap-10">
+            <div className="text-center font-extrabold text-4xl">
+                HOME
+            </div>
+            <button
+                onClick={handleClick}
+            >
+                here too
+            </button>
+            <Button
+                className="bg-bgDArk"
+                onClick={handleClick}
+            >
+                Click me
+            </Button>
+        </div>
     );
 };
 
